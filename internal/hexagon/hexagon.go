d7f73db7551a9eae974b9f6b36beea576cfcc379
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 22:30:43 +0200

    Diverse Fixes

--- internal/hexagon/hexagon.go
+++ internal/hexagon/hexagon.go
@@ -5,100 +5,140 @@ import (
 	"github.com/CmdSoda/boardgamewars/internal/segment"
 )
 
-type hexagon struct {
-	column int
-	row    int
+type Hexagon struct {
+	Column int
+	Row    int
+}
+
+func NewHexagon(c int, r int) Hexagon {
+	return Hexagon{
+		Column: c,
+		Row:    r,
+	}
 }
 
 // GetCenterCoordinates ermittelt den Mittelpunkt eines hexagons.
-func GetCenterCoordinates(h hexagon) math.Vector2 {
+func (h Hexagon) GetCenterCoordinates(useoffset bool) math.Vector2 {
 	center := math.Vector2{}
+	var offsetValue float64 = 0
+	if useoffset {
+		offsetValue = 0.1
+	}
 
-	if h.row%2 == 0 {
-		center.X = math.Hr + float64(h.column)*float64(2)*math.Hr
-		center.Y = 1 + 1.5*float64(h.row)
+	if h.Row%2 == 0 {
+		center.X = 2*math.Hr + 2*math.Hr*float64(h.Column) + offsetValue
+		center.Y = 2.5 + float64(h.Row-1)*1.5 + offsetValue
 	} else {
-		center.X = 2*math.Hr + 2*math.Hr*float64(h.column)
-		center.Y = 2.5 + (float64(h.row)-1)*1.5
+		center.X = math.Hr + float64(h.Column)*2*math.Hr + offsetValue
+		center.Y = 1 + 1.5*float64(h.Row) + offsetValue
 	}
 
 	return center
 }
 
-func (h hexagon) GetSegments() [6]segment.Segment {
+func (h Hexagon) GetSegments() [6]segment.Segment {
 	segments := [6]segment.Segment{}
 	for i := 0; i < len(segments); i++ {
-		segments[i] = segment.NewSegment(segment.Direction(i), GetCenterCoordinates(h))
+		segments[i] = segment.NewSegment(segment.Direction(i), h.GetCenterCoordinates(true))
 	}
 	return segments
 }
 
-func (h hexagon) GetAdjacent(direction segment.Direction) hexagon {
+func (h Hexagon) GetAdjacent(direction segment.Direction) *Hexagon {
+	retHex := Hexagon{}
 	switch direction {
 	case segment.NW:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column - 1,
-				row:    h.row - 1,
+		if h.Row%2 == 0 {
+			retHex = Hexagon{
+				Column: h.Column,
+				Row:    h.Row + 1,
 			}
 		} else {
-			return hexagon{
-				column: h.column,
-				row:    h.row - 1,
+			retHex = Hexagon{
+				Column: h.Column - 1,
+				Row:    h.Row + 1,
 			}
 		}
 	case segment.NE:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column,
-				row:    h.row - 1,
+		if h.Row%2 == 0 {
+			retHex = Hexagon{
+				Column: h.Column + 1,
+				Row:    h.Row + 1,
 			}
 		} else {
-			return hexagon{
-				column: h.column + 1,
-				row:    h.row - 1,
+			retHex = Hexagon{
+				Column: h.Column,
+				Row:    h.Row + 1,
 			}
 		}
 	case segment.E:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column + 1,
-				row:    h.row,
-			}
+		retHex = Hexagon{
+			Column: h.Column + 1,
+			Row:    h.Row,
 		}
 	case segment.SE:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column,
-				row:    h.row + 1,
+		if h.Row%2 == 0 {
+			retHex = Hexagon{
+				Column: h.Column + 1,
+				Row:    h.Row - 1,
 			}
 		} else {
-			return hexagon{
-				column: h.column + 1,
-				row:    h.row + 1,
+			retHex = Hexagon{
+				Column: h.Column,
+				Row:    h.Row - 1,
 			}
 		}
 	case segment.SW:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column - 1,
-				row:    h.row + 1,
+		if h.Row%2 == 0 {
+			retHex = Hexagon{
+				Column: h.Column,
+				Row:    h.Row - 1,
 			}
 		} else {
-			return hexagon{
-				column: h.column,
-				row:    h.row + 1,
+			retHex = Hexagon{
+				Column: h.Column - 1,
+				Row:    h.Row - 1,
 			}
 		}
 	case segment.W:
-		if h.row%2 == 0 {
-			return hexagon{
-				column: h.column - 1,
-				row:    h.row,
-			}
+		retHex = Hexagon{
+			Column: h.Column - 1,
+			Row:    h.Row,
 		}
-	default:
-		return hexagon{}
 	}
-	return hexagon{}
+
+	if &retHex == nil || retHex.Column < 0 || retHex.Row < 0 {
+		return nil
+	}
+
+	return &retHex
+}
+
+func (h Hexagon) Equal(h2 Hexagon) bool {
+	return h.Row == h2.Row && h.Column == h2.Column
+}
+
+func (h Hexagon) CalculateIntersectionCount(startHex Hexagon, endHex Hexagon) int {
+	segments := h.GetSegments()
+	count := 0
+	for _, s := range segments {
+		k1 := CalculateIntersectionScalar(startHex.GetCenterCoordinates(true), endHex.GetCenterCoordinates(true).Minus(startHex.GetCenterCoordinates(true)), s.Start, s.End.Minus(s.Start))
+		k2 := CalculateIntersectionScalar(s.Start, s.End.Minus(s.Start), startHex.GetCenterCoordinates(true), endHex.GetCenterCoordinates(true).Minus(startHex.GetCenterCoordinates(true)))
+		if k1 >= 0 && k2 >= 0 && k1 <= 1 && k2 <= 1 {
+			count = count + 1
+		}
+	}
+	return count
+}
+
+func CalculateIntersectionScalar(start1 math.Vector2, direction1 math.Vector2, start2 math.Vector2, direction2 math.Vector2) float64 {
+	return (direction1.Y*start1.X - direction1.X*start1.Y - direction1.Y*start2.X + direction1.X*start2.Y) / (direction1.Y*direction2.X - direction1.X*direction2.Y)
+}
+
+func CalculateDistancePointLine(point math.Vector2, lineStart math.Vector2, lineEnd math.Vector2) float64 {
+	p3 := math.NewVector3(point.X, point.Y, 0)
+	start3 := math.NewVector3(lineStart.X, lineStart.Y, 0)
+	end3 := math.NewVector3(lineEnd.X, lineEnd.Y, 0)
+	b := end3.Sub(start3)
+	return b.Cross(p3.Sub(start3)).Norm() / b.Norm()
 }
--- internal/hexagon/hexagon_test.go
+++ internal/hexagon/hexagon_test.go
@@ -1,131 +1,15 @@
 package hexagon
 
 import (
-	"github.com/CmdSoda/boardgamewars/internal/math"
 	"github.com/CmdSoda/boardgamewars/internal/segment"
 	"reflect"
 	"testing"
 )
 
-func TestGetCenterCoordinates(t *testing.T) {
-	type args struct {
-		h hexagon
-	}
-	tests := []struct {
-		name string
-		args args
-		want math.Vector2
-	}{
-		{"0", args{h: hexagon{
-			column: 0,
-			row:    0,
-		}}, math.Vector2{X: 0.8660254037844386, Y: 1}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetCenterCoordinates(tt.args.h); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetCenterCoordinates() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGetSegments(t *testing.T) {
-	type fields struct {
-		column int
-		row    int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   [6]segment.Segment
-	}{
-		{
-			name: "01",
-			fields: fields{
-				column: 0,
-				row:    0,
-			},
-			want: [6]segment.Segment{{
-				// NW
-				Start: math.Vector2{
-					X: 0,
-					Y: 0.5,
-				},
-				End: math.Vector2{
-					X: 0.8660254037844386,
-					Y: 0,
-				},
-			}, {
-				// NE
-				Start: math.Vector2{
-					X: 0.8660254037844386,
-					Y: 0,
-				},
-				End: math.Vector2{
-					X: 1.7320508075688772,
-					Y: 0.5,
-				},
-			}, {
-				// E
-				Start: math.Vector2{
-					X: 1.7320508075688772,
-					Y: 0.5,
-				},
-				End: math.Vector2{
-					X: 1.7320508075688772,
-					Y: 1.5,
-				},
-			}, {
-				// SE
-				Start: math.Vector2{
-					X: 1.7320508075688772,
-					Y: 1.5,
-				},
-				End: math.Vector2{
-					X: 0.8660254037844386,
-					Y: 2,
-				},
-			}, {
-				// SW
-				Start: math.Vector2{
-					X: 0.8660254037844386,
-					Y: 2,
-				},
-				End: math.Vector2{
-					X: 0,
-					Y: 1.5,
-				},
-			}, {
-				// W
-				Start: math.Vector2{
-					X: 0,
-					Y: 1.5,
-				},
-				End: math.Vector2{
-					X: 0,
-					Y: 0.5,
-				},
-			}},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			h := hexagon{
-				column: tt.fields.column,
-				row:    tt.fields.row,
-			}
-			if got := h.GetSegments(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetSegments() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestGetAdjacent(t *testing.T) {
 	type fields struct {
-		column int
-		row    int
+		Column int
+		Row    int
 	}
 	type args struct {
 		direction segment.Direction
@@ -134,86 +18,86 @@ func TestGetAdjacent(t *testing.T) {
 		name   string
 		fields fields
 		args   args
-		want   hexagon
+		want   *Hexagon
 	}{
 		{
 			name: "NE",
 			fields: fields{
-				column: 0,
-				row:    1,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.NE},
-			want: hexagon{
-				column: 1,
-				row:    0,
+			want: &Hexagon{
+				Column: 3,
+				Row:    3,
 			},
 		},
 		{
 			name: "E",
 			fields: fields{
-				column: 0,
-				row:    0,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.E},
-			want: hexagon{
-				column: 1,
-				row:    0,
+			want: &Hexagon{
+				Column: 3,
+				Row:    2,
 			},
 		},
 		{
 			name: "SE",
 			fields: fields{
-				column: 0,
-				row:    0,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.SE},
-			want: hexagon{
-				column: 0,
-				row:    1,
+			want: &Hexagon{
+				Column: 3,
+				Row:    1,
 			},
 		},
 		{
 			name: "SW",
 			fields: fields{
-				column: 1,
-				row:    0,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.SW},
-			want: hexagon{
-				column: 0,
-				row:    1,
+			want: &Hexagon{
+				Column: 2,
+				Row:    1,
 			},
 		},
 		{
 			name: "W",
 			fields: fields{
-				column: 1,
-				row:    0,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.W},
-			want: hexagon{
-				column: 0,
-				row:    0,
+			want: &Hexagon{
+				Column: 1,
+				Row:    2,
 			},
 		},
 		{
 			name: "NW",
 			fields: fields{
-				column: 0,
-				row:    1,
+				Column: 2,
+				Row:    2,
 			},
 			args: args{direction: segment.NW},
-			want: hexagon{
-				column: 0,
-				row:    0,
+			want: &Hexagon{
+				Column: 2,
+				Row:    3,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := hexagon{
-				column: tt.fields.column,
-				row:    tt.fields.row,
+			h := Hexagon{
+				Column: tt.fields.Column,
+				Row:    tt.fields.Row,
 			}
 			if got := h.GetAdjacent(tt.args.direction); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetAdjacent() = %v, want %v", got, tt.want)
