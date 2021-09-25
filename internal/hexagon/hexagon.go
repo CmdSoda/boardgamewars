294c4ce7d3fa7306a9d7811586ca647aecd944a6
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 17:59:18 +0200

    Hr gefixt

--- internal/hexagon/hexagon.go
+++ internal/hexagon/hexagon.go
@@ -12,10 +12,10 @@ func GetCenterCoordinates(h hexagon) math.Vector2 {
 	center := math.Vector2{}
 
 	if h.row%2 == 0 {
-		center.X = math.hr + float64(h.column)*float64(2)*math.hr
+		center.X = math.Hr + float64(h.column)*float64(2)*math.Hr
 		center.Y = 1 + 1.5*float64(h.row)
 	} else {
-		center.X = 2*math.hr + 2*math.hr*float64(h.column)
+		center.X = 2*math.Hr + 2*math.Hr*float64(h.column)
 		center.Y = 2.5 + (float64(h.row)-1)*1.5
 	}
 
