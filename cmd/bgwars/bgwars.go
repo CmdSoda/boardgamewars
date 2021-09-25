b8bd7bb933e0ef98519cb5dff852a7e0955c8a1c
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 22:45:03 +0200

    CalculatePath ausgabe

--- cmd/bgwars/bgwars.go
+++ cmd/bgwars/bgwars.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/CmdSoda/boardgamewars/internal/hexagon"
+	"github.com/CmdSoda/boardgamewars/internal/path"
+)
 
 func main() {
 	fmt.Println("bgwars")
+	calcpath := path.CalculatePath(hexagon.NewHexagon(1, 1), hexagon.NewHexagon(7, 3))
+	fmt.Println(calcpath)
 }
