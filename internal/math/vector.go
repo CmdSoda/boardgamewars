edfc840aea9a0f065b33abf9121bed807cd180ad
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 22:30:59 +0200

    Vector2 und Vector3

--- internal/math/vector.go
+++ internal/math/vector.go
@@ -1,6 +1,67 @@
 package math
 
+import "math"
+
+//
+// Vector 2
+//
+
 type Vector2 struct {
 	X float64
 	Y float64
 }
+
+//goland:noinspection ALL
+func NewVector2(x float64, y float64) Vector2 {
+	return Vector2{
+		X: x,
+		Y: y,
+	}
+}
+
+func (v Vector2) Minus(v2 Vector2) Vector2 {
+	return Vector2{
+		X: v.X - v2.X,
+		Y: v.Y - v2.Y,
+	}
+}
+
+//
+// Vector 3
+//
+
+type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func NewVector3(x float64, y float64, z float64) Vector3 {
+	return Vector3{
+		X: x,
+		Y: y,
+		Z: z,
+	}
+}
+
+func (v Vector3) Sub(v2 Vector3) Vector3 {
+	return Vector3{
+		X: v.X - v2.X,
+		Y: v.Y - v2.Y,
+		Z: v.Z - v2.Z,
+	}
+}
+
+func (v Vector3) Cross(ov Vector3) Vector3 {
+	return Vector3{
+		v.Y*ov.Z - v.Z*ov.Y,
+		v.Z*ov.X - v.X*ov.Z,
+		v.X*ov.Y - v.Y*ov.X,
+	}
+}
+
+func (v Vector3) Distance(ov Vector3) float64 { return v.Sub(ov).Norm() }
+
+func (v Vector3) Norm() float64 { return math.Sqrt(v.Dot(v)) }
+
+func (v Vector3) Dot(ov Vector3) float64 { return v.X*ov.X + v.Y*ov.Y + v.Z*ov.Z }
