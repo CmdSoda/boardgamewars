d15897cd1f50f797f398a822a2585ed62197cea3
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 19:29:18 +0200

    UT GetAdjacent

--- internal/hexagon/hexagon_test.go
+++ internal/hexagon/hexagon_test.go
@@ -121,3 +121,103 @@ func TestGetSegments(t *testing.T) {
 		})
 	}
 }
+
+func TestGetAdjacent(t *testing.T) {
+	type fields struct {
+		column int
+		row    int
+	}
+	type args struct {
+		direction segment.Direction
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   hexagon
+	}{
+		{
+			name: "NE",
+			fields: fields{
+				column: 0,
+				row:    1,
+			},
+			args: args{direction: segment.NE},
+			want: hexagon{
+				column: 1,
+				row:    0,
+			},
+		},
+		{
+			name: "E",
+			fields: fields{
+				column: 0,
+				row:    0,
+			},
+			args: args{direction: segment.E},
+			want: hexagon{
+				column: 1,
+				row:    0,
+			},
+		},
+		{
+			name: "SE",
+			fields: fields{
+				column: 0,
+				row:    0,
+			},
+			args: args{direction: segment.SE},
+			want: hexagon{
+				column: 0,
+				row:    1,
+			},
+		},
+		{
+			name: "SW",
+			fields: fields{
+				column: 1,
+				row:    0,
+			},
+			args: args{direction: segment.SW},
+			want: hexagon{
+				column: 0,
+				row:    1,
+			},
+		},
+		{
+			name: "W",
+			fields: fields{
+				column: 1,
+				row:    0,
+			},
+			args: args{direction: segment.W},
+			want: hexagon{
+				column: 0,
+				row:    0,
+			},
+		},
+		{
+			name: "NW",
+			fields: fields{
+				column: 0,
+				row:    1,
+			},
+			args: args{direction: segment.NW},
+			want: hexagon{
+				column: 0,
+				row:    0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := hexagon{
+				column: tt.fields.column,
+				row:    tt.fields.row,
+			}
+			if got := h.GetAdjacent(tt.args.direction); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAdjacent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
