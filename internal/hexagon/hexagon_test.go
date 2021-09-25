6f63450a2d5339b3ff68eaccb90aa5ed444cc8ca
Patrick Lüdke <patrick.luedke@acticom.de>
2021-09-25 18:47:24 +0200

    UT für GetSegments

--- internal/hexagon/hexagon_test.go
+++ internal/hexagon/hexagon_test.go
@@ -2,6 +2,7 @@ package hexagon
 
 import (
 	"github.com/CmdSoda/boardgamewars/internal/math"
+	"github.com/CmdSoda/boardgamewars/internal/segment"
 	"reflect"
 	"testing"
 )
@@ -24,6 +25,98 @@ func TestGetCenterCoordinates(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetCenterCoordinates(tt.args.h); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetCenterCoordinates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetSegments(t *testing.T) {
+	type fields struct {
+		column int
+		row    int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   [6]segment.Segment
+	}{
+		{
+			name: "01",
+			fields: fields{
+				column: 0,
+				row:    0,
+			},
+			want: [6]segment.Segment{{
+				// NW
+				Start: math.Vector2{
+					X: 0,
+					Y: 0.5,
+				},
+				End: math.Vector2{
+					X: 0.8660254037844386,
+					Y: 0,
+				},
+			}, {
+				// NE
+				Start: math.Vector2{
+					X: 0.8660254037844386,
+					Y: 0,
+				},
+				End: math.Vector2{
+					X: 1.7320508075688772,
+					Y: 0.5,
+				},
+			}, {
+				// E
+				Start: math.Vector2{
+					X: 1.7320508075688772,
+					Y: 0.5,
+				},
+				End: math.Vector2{
+					X: 1.7320508075688772,
+					Y: 1.5,
+				},
+			}, {
+				// SE
+				Start: math.Vector2{
+					X: 1.7320508075688772,
+					Y: 1.5,
+				},
+				End: math.Vector2{
+					X: 0.8660254037844386,
+					Y: 2,
+				},
+			}, {
+				// SW
+				Start: math.Vector2{
+					X: 0.8660254037844386,
+					Y: 2,
+				},
+				End: math.Vector2{
+					X: 0,
+					Y: 1.5,
+				},
+			}, {
+				// W
+				Start: math.Vector2{
+					X: 0,
+					Y: 1.5,
+				},
+				End: math.Vector2{
+					X: 0,
+					Y: 0.5,
+				},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := hexagon{
+				column: tt.fields.column,
+				row:    tt.fields.row,
+			}
+			if got := h.GetSegments(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetSegments() = %v, want %v", got, tt.want)
 			}
 		})
 	}
