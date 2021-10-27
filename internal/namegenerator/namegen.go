31803277d14644f2038c0a0547a7a6fe6c94ccfd
Patrick Lüdke <patrick.luedke@acticom.de>
2021-10-27 21:13:34 +0200

    airbases

--- internal/namegenerator/namegen.go
+++ internal/namegenerator/namegen.go
@@ -18,36 +18,41 @@ func ignoreBraces(s string) string {
 }
 
 type NameSet struct {
-	Males    []string
-	Females  []string
-	Surnames []string
-	Cities   []string
+	Males        []string
+	Females      []string
+	Surnames     []string
+	Cities       []string
+	AirForceBase []string
 }
 
 var theNameSet = map[countrycodes.Code]NameSet{
 	countrycodes.UK: {
-		Males:    country.MaleFirstNamesUK,
-		Females:  country.FemaleFirstNamesUK,
-		Surnames: country.SurnamesUK,
-		Cities:   country.CitiesUK,
+		Males:        country.MaleFirstNamesUK,
+		Females:      country.FemaleFirstNamesUK,
+		Surnames:     country.SurnamesUK,
+		Cities:       country.CitiesUK,
+		AirForceBase: country.AirForceBasesUK,
 	},
 	countrycodes.Germany: {
-		Males:    country.MaleFirstNamesGermany,
-		Females:  country.FemaleFirstNamesGermany,
-		Surnames: country.SurnamesGermany,
-		Cities:   country.CitiesGermany,
+		Males:        country.MaleFirstNamesGermany,
+		Females:      country.FemaleFirstNamesGermany,
+		Surnames:     country.SurnamesGermany,
+		Cities:       country.CitiesGermany,
+		AirForceBase: country.AirForceBasesGermany,
 	},
 	countrycodes.USA: {
-		Males:    country.MaleFirstNamesUSA,
-		Females:  country.FemaleFirstNamesUSA,
-		Surnames: country.SurnamesUSA,
-		Cities:   country.CitiesUSA,
+		Males:        country.MaleFirstNamesUSA,
+		Females:      country.FemaleFirstNamesUSA,
+		Surnames:     country.SurnamesUSA,
+		Cities:       country.CitiesUSA,
+		AirForceBase: country.AirForceBasesUSA,
 	},
 	countrycodes.Russia: {
-		Males:    country.MaleFirstNamesRussia,
-		Females:  country.FemaleFirstNamesRussia,
-		Surnames: country.SurnamesRussia,
-		Cities:   country.CitiesRussia,
+		Males:        country.MaleFirstNamesRussia,
+		Females:      country.FemaleFirstNamesRussia,
+		Surnames:     country.SurnamesRussia,
+		Cities:       country.CitiesRussia,
+		AirForceBase: country.AirForceBasesRussia,
 	},
 }
 
@@ -70,5 +75,11 @@ func CreateFemaleFullName(cc countrycodes.Code) string {
 func CreateCityName(cc countrycodes.Code) string {
 	r := randomizer.Roll1DN(len(theNameSet[cc].Cities))
 	city := ignoreBraces(strings.Title(strings.ToLower(theNameSet[cc].Cities[r-1])))
+	return city
+}
+
+func CreateAirForceBaseName(cc countrycodes.Code) string {
+	r := randomizer.Roll1DN(len(theNameSet[cc].AirForceBase))
+	city := ignoreBraces(strings.Title(strings.ToLower(theNameSet[cc].AirForceBase[r-1])))
 	return city
 }
