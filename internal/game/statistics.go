16ec3648c6e7194f335b94b03bcace1c93663598
Patrick Lüdke <patrick.luedke@acticom.de>
2021-11-19 00:08:57 +0100

    ch

--- internal/game/statistics.go
+++ internal/game/statistics.go
@@ -1,5 +1,7 @@
 package game
 
+import "fmt"
+
 // WeaponNameVsAircraftParameterIdMap ist eine Map auf Waffensystem-Name auf AircraftParametersId auf Config-Name
 // Beispiel:
 // m := WeaponNameVsAircraftParameterIdMap{}
@@ -12,58 +14,73 @@ type WeaponStatistics struct {
 }
 
 type Statistics struct {
-	WeaponNameVsAircraftParameterIdMap
+	W2A2C WeaponNameVsAircraftParameterIdMap
 }
 
 func NewStatistics() Statistics {
 	s := Statistics{}
-	s.WeaponNameVsAircraftParameterIdMap = map[string]map[AircraftParametersId]map[string]*WeaponStatistics{}
+	s.W2A2C = map[string]map[AircraftParametersId]map[string]*WeaponStatistics{}
 	return s
+}
+
+func (wmap WeaponNameVsAircraftParameterIdMap) Dump() {
+	for wname := range wmap {
+		fmt.Println(wname)
+		for acid := range wmap[wname] {
+			acp := Globals.AllAircraftParameters[acid]
+			fmt.Println("  " + acp.Name)
+			for config := range wmap[wname][acid] {
+				hitpc := float32(wmap[wname][acid][config].Hit) /
+					(float32(wmap[wname][acid][config].Hit) + float32(wmap[wname][acid][config].NotHit)) * 100
+				fmt.Printf("    %s Hit%% %f\n", config, hitpc)
+			}
+		}
+	}
 }
 
 func (s *Statistics) Hit(weaponName string, acid AircraftId) {
 	ac := Globals.AllAircrafts[acid]
 
-	_, wnexist := s.WeaponNameVsAircraftParameterIdMap[weaponName]
+	_, wnexist := s.W2A2C[weaponName]
 	if !wnexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName] = map[AircraftParametersId]map[string]*WeaponStatistics{}
+		s.W2A2C[weaponName] = map[AircraftParametersId]map[string]*WeaponStatistics{}
 	}
 
-	_, apidexist := s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId]
+	_, apidexist := s.W2A2C[weaponName][ac.AircraftParametersId]
 	if !apidexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId] = map[string]*WeaponStatistics{}
+		s.W2A2C[weaponName][ac.AircraftParametersId] = map[string]*WeaponStatistics{}
 	}
 
-	_, cexist := s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName]
+	_, cexist := s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName]
 	if !cexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName] = &WeaponStatistics{}
+		s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName] = &WeaponStatistics{}
 	}
 
 	// Um 1 erhöhen.
-	s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].Hit =
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].Hit +1
+	s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].Hit =
+		s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].Hit +1
 }
 
 func (s *Statistics) NotHit(weaponName string, acid AircraftId) {
 	ac := Globals.AllAircrafts[acid]
 
-	_, wnexist := s.WeaponNameVsAircraftParameterIdMap[weaponName]
+	_, wnexist := s.W2A2C[weaponName]
 	if !wnexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName] = map[AircraftParametersId]map[string]*WeaponStatistics{}
+		s.W2A2C[weaponName] = map[AircraftParametersId]map[string]*WeaponStatistics{}
 	}
 
-	_, apidexist := s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId]
+	_, apidexist := s.W2A2C[weaponName][ac.AircraftParametersId]
 	if !apidexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId] = map[string]*WeaponStatistics{}
+		s.W2A2C[weaponName][ac.AircraftParametersId] = map[string]*WeaponStatistics{}
 	}
 
-	_, cexist := s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName]
+	_, cexist := s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName]
 	if !cexist {
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName] = &WeaponStatistics{}
+		s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName] = &WeaponStatistics{}
 	}
 
 	// Um 1 erhöhen.
-	s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].NotHit =
-		s.WeaponNameVsAircraftParameterIdMap[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].NotHit +1
+	s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].NotHit =
+		s.W2A2C[weaponName][ac.AircraftParametersId][ac.WeaponsConfigName].NotHit +1
 }
 
--- internal/game/statistics_test.go
+++ internal/game/statistics_test.go
@@ -15,8 +15,10 @@ func TestStatistics_Hit(t *testing.T) {
 	s.Hit("Aim-7", ac.AircraftId)
 	s.NotHit("Aim-7", ac.AircraftId)
 	s.Hit("Aim-9", ac.AircraftId)
-	assert.Equal(t, 3, s.WeaponNameVsAircraftParameterIdMap["Aim-7"][ac.AircraftParametersId]["Default"].Hit)
-	assert.Equal(t, 1, s.WeaponNameVsAircraftParameterIdMap["Aim-7"][ac.AircraftParametersId]["Default"].NotHit)
-	assert.Equal(t, 1, s.WeaponNameVsAircraftParameterIdMap["Aim-9"][ac.AircraftParametersId]["Default"].Hit)
-	assert.Equal(t, 0, s.WeaponNameVsAircraftParameterIdMap["Aim-9"][ac.AircraftParametersId]["Default"].NotHit)
+	assert.Equal(t, 3, s.W2A2C["Aim-7"][ac.AircraftParametersId]["Default"].Hit)
+	assert.Equal(t, 1, s.W2A2C["Aim-7"][ac.AircraftParametersId]["Default"].NotHit)
+	assert.Equal(t, 1, s.W2A2C["Aim-9"][ac.AircraftParametersId]["Default"].Hit)
+	assert.Equal(t, 0, s.W2A2C["Aim-9"][ac.AircraftParametersId]["Default"].NotHit)
+
+	s.W2A2C.Dump()
 }
