25dcffe28e0d42ad2ecd16226751dcd7a2dd3910
Patrick Lüdke <patrick.luedke@acticom.de>
2021-11-21 17:36:58 +0100

    ch

--- internal/game/world.go
+++ internal/game/world.go
@@ -3,12 +3,18 @@ package game
 import "fmt"
 
 type World struct {
+	CurrentStep StepTime
 }
 
 func NewWorld() World {
 	return World{}
 }
 
-func (w World) Step(st StepTime) {
+func (w *World) Step(st StepTime) {
+	w.CurrentStep = w.CurrentStep + st
 	fmt.Println(st)
+}
+
+func Step(st StepTime) {
+	Globals.World.Step(st)
 }
--- internal/game/world_test.go
+++ internal/game/world_test.go
@@ -2,10 +2,18 @@ package game
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewWorld(t *testing.T) {
 	w := NewWorld()
 	fmt.Println(w)
+}
+
+func TestStep(t *testing.T) {
+	assert.Nil(t, InitGameWithLogLevel(0, logrus.WarnLevel))
+	Step(3)
+	assert.Equal(t, StepTime(3), Globals.World.CurrentStep)
 }
