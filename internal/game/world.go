9fd8a85aaeeb849faa4592723104afe0374e20c9
Patrick Lüdke <patrick.luedke@acticom.de>
2021-11-29 22:13:14 +0100

    ch

--- internal/game/world.go
+++ internal/game/world.go
@@ -3,24 +3,34 @@ package game
 import "fmt"
 
 type World struct {
-	CurrentStep StepTime
-	Machines    []*Stepper
+	CurrentStep       StepTime
+	Airbases          []AirbaseId
+	AircraftsInTheAir []AircraftId
 }
 
 func NewWorld() World {
-	return World{Machines: []*Stepper{}}
+	return World{Airbases: []AirbaseId{}}
 }
 
-func (w *World) Add(st *Stepper) {
+func (w *World) AddAirbase(st AirbaseId) {
 	//var s Stepper = st
-	w.Machines = append(w.Machines, st)
+	w.Airbases = append(w.Airbases, st)
+}
+
+func (w *World) MoveAircraftToAir(acid AircraftId) {
+
+}
+
+func (w *World) MoveAircraftToAirbase(acid AircraftId) {
+
 }
 
 func (w *World) Step(st StepTime) {
 	w.CurrentStep = w.CurrentStep + st
 	fmt.Println(st)
-	for _, machine := range w.Machines {
-		(*machine).Step(st)
+	for _, abid := range w.Airbases {
+		ab := Globals.AllAirbases[abid]
+		ab.Step(st)
 	}
 }
 
--- internal/game/world_test.go
+++ internal/game/world_test.go
@@ -3,7 +3,6 @@ package game
 import (
 	"fmt"
 	"github.com/CmdSoda/boardgamewars/internal/hexagon"
-	"github.com/CmdSoda/boardgamewars/internal/nato"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -22,21 +21,9 @@ func TestStep(t *testing.T) {
 	assert.Equal(t, StepTime(10), Globals.World.CurrentStep)
 }
 
-func TestAircraftStep(t *testing.T) {
-	assert.Nil(t, InitGameWithLogLevel(0, logrus.WarnLevel))
-	ac1 := NewAircraft("F14", "Default", WarPartyIdUSA)
-	ac1.FillSeatsWithNewPilots(nato.OF1)
-	var st Stepper = ac1
-	Globals.World.Add(&st)
-	Globals.World.Step(3)
-	assert.Equal(t, StepTime(3), ac1.StepsTaken)
-}
-
 func TestAirbaseSteps(t *testing.T) {
 	assert.Nil(t, InitGameWithLogLevel(0, logrus.WarnLevel))
 	ab := NewAirbase("Airbase 1", WarPartyIdUSA, hexagon.NewHexagon(15, 15))
-	var st Stepper = ab
-	Globals.World.Add(&st)
+	Globals.World.AddAirbase(ab.AirbaseId)
 	Globals.World.Step(1)
-
 }
